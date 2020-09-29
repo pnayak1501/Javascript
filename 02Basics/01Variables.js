52d9dea8fce846a88499ea36340f67e7d62cdf30
Prahlad Nayak <pnayak1501@gmail.com>
2020-09-29 23:31:18 +0530

    Add files via upload

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,17 @@
+var fullName = "Prahlad Nayak";
+
+//how to name variables
+//full-name
+//full_name
+// FullName
+// We cant name the variables by the name of reserved keyword
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = true;
+
+var loggedCount = 34;
+console.log(loggedCount);
+
+var paymentMode;
+console.log(paymentMode); // this will be undefined
+paymentMode = "Credit Card";

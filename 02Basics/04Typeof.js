9d743104bda2fe0905dc5f585c3954e1b5987ad9
Prahlad Nayak <pnayak1501@gmail.com>
2020-10-01 00:05:18 +0530

    Add files via upload

---
+++ 02Basics/04Typeof.js
@@ -0,0 +1,8 @@
+var ls = 23;
+var sp = 54;
+var str = "Prahlad";
+
+var result = ls > sp;
+console.log(typeof result); //returns the typeof the variable
+console.log(typeof sp);
+console.log(typeof str);

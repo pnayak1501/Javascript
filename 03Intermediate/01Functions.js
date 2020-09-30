2395337a9469aed3db253a55b8ed010c28586436
Prahlad Nayak <pnayak1501@gmail.com>
2020-10-01 00:08:44 +0530

    Add files via upload

---
+++ 03Intermediate/01Functions.js
@@ -0,0 +1,22 @@
+// function sayHello(){
+//     console.log("Heloo there!");
+// }
+
+function sayHello(name){
+    console.log(`Heloo there, ${name}!`);
+}
+
+
+sayHello("Parag");
+sayHello("Prahlad");
+
+
+//Returning from functions
+function namstey(){
+    return "Hello in India!";
+}
+
+var greet = namstey();
+console.log(greet);
+console.log(namstey());
+

3dd658824852a60ff757f18796c197825fc58a25
Prahlad Nayak <pnayak1501@gmail.com>
2020-10-01 00:08:05 +0530

    Add files via upload

---
+++ 02Basics/11Coercion.js
@@ -0,0 +1,9 @@
+var user = "2";
+
+if(2 == user){ //This is coercion (loose conditional)
+    console.log("Condition is true");
+}
+
+if(2 === user){// Strict conditionals
+    console.log("Condition is true");
+}

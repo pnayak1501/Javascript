6c9db615a26948853b253cb4c436906ea2cb2d38
Prahlad Nayak <pnayak1501@gmail.com>
2020-10-01 00:05:54 +0530

    Add files via upload

---
+++ 02Basics/05Conditionals.js
@@ -0,0 +1,13 @@
+var temperature;
+temperature = 20;
+
+if(temperature<20){
+    console.log("It's very cold outside");
+}
+
+if(temperature<30){
+    console.log("It's moderate outside");
+}
+else{
+    console.log("It's really hot outside");
+}

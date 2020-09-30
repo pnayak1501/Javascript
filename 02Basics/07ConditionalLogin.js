61e13927eabcaddc2d8088d2c36821c1b3172463
Prahlad Nayak <pnayak1501@gmail.com>
2020-10-01 00:06:49 +0530

    Add files via upload

---
+++ 02Basics/07ConditionalLogin.js
@@ -0,0 +1,11 @@
+var email = true;
+var facebook = false;
+var google = false;
+
+// if(email){
+//     console.log("Login success");
+// }
+
+if(email || facebook || google){
+    console.log("Login success");
+}

cd737514e832455210d857fe6dd56a59605cd1ad
Prahlad Nayak <pnayak1501@gmail.com>
2020-10-01 17:35:21 +0530

    Add files via upload

---
+++ 03Intermediate/09FilterArray.js
@@ -0,0 +1,9 @@
+var testArray = [2,4,6,3,1,5,9,8];
+// console.log(testArray.fill("h")); //fill function replaces all the elements of the array with a particular value
+// console.log(testArray.fill("h",2,5));
+
+const myNumbers = [23,24,25,55,66,77,87,98];
+
+const result = myNumbers.filter((num)=> num<55 ); //Removing anything using some logic
+console.log(result);
+

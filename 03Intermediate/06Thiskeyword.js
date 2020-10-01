c1a4248c144abdcb630f443f805bf0b22781c2ce
Prahlad Nayak <pnayak1501@gmail.com>
2020-10-01 17:34:46 +0530

    Add files via upload

---
+++ 03Intermediate/06Thiskeyword.js
@@ -0,0 +1,12 @@
+//TODO: Part 1
+
+console.log(this); // We get window on a browser, as it is the global context
+
+var game = "basketball";
+
+// function sayName(){
+//     var name = "Prahlad";
+//     console.log(this);
+// }
+
+// sayName();
